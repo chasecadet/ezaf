c2c0cab8c27b9d79286bedcf2bba97fe19d8a3ce
seechasesee <christensenc3526@gmail.com>
2023-09-20 01:47:23 +0000

    fixing script and component

--- pipelines/v1 /containerized_python_components/digit-recognition-kaggle-competition/components/consolidated_steps/download_load_preprocess_data/.ipynb_checkpoints/download_load_preprocess_data-checkpoint.py
+++ pipelines/v1 /containerized_python_components/digit-recognition-kaggle-competition/components/consolidated_steps/download_load_preprocess_data/.ipynb_checkpoints/download_load_preprocess_data-checkpoint.py
@@ -12,26 +12,31 @@ def _make_parent_dirs_and_return_path(file_path: str):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     return file_path
 
-def main(argv=None):
+
+def main():
     print("oh here we goooooo")
-    _parser = argparse.ArgumentParser(prog='Download load preprocess data', description='')
-    _parser.add_argument("--download-link", dest="download_link", type=str, required=True, default=argparse.SUPPRESS)
-    _parser.add_argument("--output-data", dest="output_data_path", type=_make_parent_dirs_and_return_path, required=True, default=argparse.SUPPRESS)
-    _parsed_args = vars(_parser.parse_args())
-    _outputs = main(**_parsed_args)    
+    parser = argparse.ArgumentParser(prog='Download load preprocess data', description='')
+    parser.add_argument("--download-link", dest="download_link", type=str, required=True)
+    parser.add_argument("--output-data", dest="output_data_path", type=_make_parent_dirs_and_return_path, required=True)
+    args = parser.parse_args()
+
     # Check and create output data path
-    if not os.path.exists(output_data_path):
-        os.makedirs(output_data_path) 
+    if not os.path.exists(args.output_data_path):
+        os.makedirs(args.output_data_path)
+
     # Step 1: Download Data
-    wget.download(download_link.format(file='train'), f'{output_data_path}/train_csv.zip')
-    wget.download(download_link.format(file='test'), f'{output_data_path}/test_csv.zip')
-    with zipfile.ZipFile(f"{output_data_path}/train_csv.zip","r") as zip_ref:
-        zip_ref.extractall(output_data_path)
-    with zipfile.ZipFile(f"{output_data_path}/test_csv.zip","r") as zip_ref:
-        zip_ref.extractall(output_data_path)
+    wget.download(args.download_link.format(file='train'), f'{args.output_data_path}/train_csv.zip')
+    wget.download(args.download_link.format(file='test'), f'{args.output_data_path}/test_csv.zip')
+    
+    with zipfile.ZipFile(f"{args.output_data_path}/train_csv.zip", "r") as zip_ref:
+        zip_ref.extractall(args.output_data_path)
+        
+    with zipfile.ZipFile(f"{args.output_data_path}/test_csv.zip", "r") as zip_ref:
+        zip_ref.extractall(args.output_data_path)
+
     # Step 2: Load Data
-    train_data_path = os.path.join(output_data_path, 'train.csv')
-    test_data_path = os.path.join(output_data_path, 'test.csv')
+    train_data_path = os.path.join(args.output_data_path, 'train.csv')
+    test_data_path = os.path.join(args.output_data_path, 'test.csv')
 
     train_df = pd.read_csv(train_data_path)
     test_df = pd.read_csv(test_data_path)
@@ -52,14 +57,13 @@ def main(argv=None):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
     
-    with open(f'{output_data_path}/train', 'wb') as f:
-        pickle.dump((X_train,  y_train), f)
+    output_data_file_path = os.path.join(args.output_data_path, 'output_data.pkl')
+    with open(output_data_file_path, 'wb') as f:
+        pickle.dump((X_train, y_train, X_test, y_test), f)
         
-    with open(f'{output_data_path}/test', 'wb') as f:
-        pickle.dump((X_test,  y_test), f)
+    print('Done!')
+    return output_data_file_path
 
-    
-    return print('Done!')
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
--- pipelines/v1 /containerized_python_components/digit-recognition-kaggle-competition/components/consolidated_steps/download_load_preprocess_data/download_load_preprocess_data.py
+++ pipelines/v1 /containerized_python_components/digit-recognition-kaggle-competition/components/consolidated_steps/download_load_preprocess_data/download_load_preprocess_data.py
@@ -3,32 +3,40 @@ import zipfile
 import pickle
 import wget
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 import argparse
+
 
 def _make_parent_dirs_and_return_path(file_path: str):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     return file_path
 
-def download_load_preprocess_data(download_link: str, output_data_path: str):
-    print("Starting data download and preprocessing...")
+
+def main():
+    print("oh here we goooooo")
+    parser = argparse.ArgumentParser(prog='Download load preprocess data', description='')
+    parser.add_argument("--download-link", dest="download_link", type=str, required=True)
+    parser.add_argument("--output-data", dest="output_data_path", type=_make_parent_dirs_and_return_path, required=True)
+    args = parser.parse_args()
 
     # Check and create output data path
-    os.makedirs(output_data_path, exist_ok=True)
+    if not os.path.exists(args.output_data_path):
+        os.makedirs(args.output_data_path)
 
     # Step 1: Download Data
-    wget.download(download_link.format(file='train'), f'{output_data_path}/train_csv.zip')
-    wget.download(download_link.format(file='test'), f'{output_data_path}/test_csv.zip')
+    wget.download(args.download_link.format(file='train'), f'{args.output_data_path}/train_csv.zip')
+    wget.download(args.download_link.format(file='test'), f'{args.output_data_path}/test_csv.zip')
     
-    with zipfile.ZipFile(f"{output_data_path}/train_csv.zip", "r") as zip_ref:
-        zip_ref.extractall(output_data_path)
-
-    with zipfile.ZipFile(f"{output_data_path}/test_csv.zip", "r") as zip_ref:
-        zip_ref.extractall(output_data_path)
+    with zipfile.ZipFile(f"{args.output_data_path}/train_csv.zip", "r") as zip_ref:
+        zip_ref.extractall(args.output_data_path)
+        
+    with zipfile.ZipFile(f"{args.output_data_path}/test_csv.zip", "r") as zip_ref:
+        zip_ref.extractall(args.output_data_path)
 
     # Step 2: Load Data
-    train_data_path = os.path.join(output_data_path, 'train.csv')
-    test_data_path = os.path.join(output_data_path, 'test.csv')
+    train_data_path = os.path.join(args.output_data_path, 'train.csv')
+    test_data_path = os.path.join(args.output_data_path, 'test.csv')
 
     train_df = pd.read_csv(train_data_path)
     test_df = pd.read_csv(test_data_path)
@@ -48,25 +56,14 @@ def download_load_preprocess_data(download_link: str, output_data_path: str):
     y = all_data_y[:ntrain].copy()
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
-    with open(f'{output_data_path}/train', 'wb') as f:
-        pickle.dump((X_train, y_train), f)
-        
-    with open(f'{output_data_path}/test', 'wb') as f:
-        pickle.dump((X_test, y_test), f)
     
+    output_data_file_path = os.path.join(args.output_data_path, 'output_data.pkl')
+    with open(output_data_file_path, 'wb') as f:
+        pickle.dump((X_train, y_train, X_test, y_test), f)
+        
     print('Done!')
+    return output_data_file_path
 
-def main(argv=None):
-    print("oh here we goooooo")
-
-    parser = argparse.ArgumentParser(prog='Download load preprocess data', description='')
-    parser.add_argument("--download-link", dest="download_link", type=str, required=True)
-    parser.add_argument("--output-data", dest="output_data_path", type=_make_parent_dirs_and_return_path, required=True)
-    parsed_args = vars(parser.parse_args())
-
-    download_load_preprocess_data(**parsed_args)
 
 if __name__ == "__main__":
     main()
-
